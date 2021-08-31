322f9254e13c033d4c6c999894c0eac5c8aa7f30
henriguy1210 <henri.idrovo@pm.me>
2021-08-31 00:56:24 -0500

    added search bar and logo.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+        <div>
+          <img
+            className="logo"
+            src="https://henri-public-bucket.s3.us-east-2.amazonaws.com/asci_img.jpg"
+            alt="CHICAGO"
+          />
+        </div>
+
+        <body>
+          <form class="example" action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search2" />
+            <button type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </form>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
