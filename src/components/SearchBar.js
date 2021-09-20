57f699b6f77709b4fcc58bcd3b119181ed010e73
henriguy1210 <henri.idrovo@pm.me>
2021-09-19 19:43:06 -0500

    added spanish dictionary api.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,14 +5,44 @@ import "./SearchBar.css";
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "enter your query here bro." };
+    this.state = { value: "enter your query here bro.", result: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    const axios = require("axios");
+
     console.log(`you submit the form tio! ${this.state.value}`);
+
+    let self = this;
+    axios
+      .get(
+        `https://dictionaryapi.com/api/v3/references/spanish/json/${this.state.value}`,
+        {
+          params: {
+            key: process.env.REACT_APP_MERRIAM_API_KEY,
+          },
+        }
+      )
+      .then(function (response) {
+        // sucess scenario
+        console.log(response);
+        let shortDefinition = response.data[0].shortdef[0];
+        console.log(shortDefinition);
+        self.setState({
+          value: event.target.value,
+          result: shortDefinition,
+        });
+      })
+      .catch(function (error) {
+        //error scenario
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   }
 
   handleChange(event) {
@@ -32,6 +62,8 @@ class SearchBar extends React.Component {
             <i class="fa fa-search"></i>
           </button>
         </form>
+        <br></br>
+        <p class="response">{this.state.result}</p>
       </div>
     );
   }
